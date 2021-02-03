db6836882c450a7d5498b28127a9d74a70384415
Niranjan Bommidi <niranjanprasad.b@gmail.com>
2021-02-03 18:05:29 +0530

    Updated app.js and index.ejs

--- src/app.js
+++ src/app.js
@@ -1,14 +1,12 @@
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
+
 const app = express();
-app.set('views', path.join(__dirname, '/src/views'));
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', function (request, response) {
-    response.render("index", { 'title': 'Index' });
-})
+app.get('/', (request, response) => response.render('index', { 'title': 'Index' }));
 
-app.listen(3000, function (request, response) {
-    console.log("PS Project Running on port 3000!");
-});
+app.listen(3000, () => console.log("PS Project Running on port 3000!"));
